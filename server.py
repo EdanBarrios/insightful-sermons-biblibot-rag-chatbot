713cdfb49226e25a5bb05db053c1568db3c9a0e3
Edan Barrios <110215725+EdanBarrios@users.noreply.github.com>
2026-03-22 16:47:10 -0700

    Fix missing parenthesis in app.run call

--- server.py
+++ server.py
@@ -237,4 +237,4 @@ def server_error(e):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False
+    app.run(host="0.0.0.0", port=port, debug=False)
